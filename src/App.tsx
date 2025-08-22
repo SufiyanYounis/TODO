2d3e639861d24d94d7478ec17e4bcf1328477766
SufiyanYounis <sufiyan.younis@mergestack.com>
2025-08-22 17:24:55 +0500

    refactor: remove warnings in the code

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import "./App.css";
-import { useMemo } from "react";
 import {Routes, Route } from "react-router-dom";
 import TodoDashboard from "./Pages/TodoDashboard";
 import LogIn from "./Pages/LogIn";
 import SignUp from "./Pages/SignUp";
 import Home from "./Pages/Home";
-import type{ RootState } from "./app/store.ts";
-import { useDispatch , useSelector} from "react-redux";
-import {addTask,DeleteTask,EditTask,Toggle} from "../src/Features/TaskSlice.tsx"
-
 const App = () => {
 
  
--- src/Components/TaskDone.tsx
+++ src/Components/TaskDone.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import TaskCount from "./TaskCount";
 
 const TaskDone = () => {
