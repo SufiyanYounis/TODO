f54b4e6cecfb61ee5bdc62d068530f9f9342e60b
SufiyanYounis <sufiyan.younis@mergestack.com>
2025-08-26 16:46:33 +0500

    delete env from github

--- Backend/src/Models/TaskModel.ts
+++ Backend/src/Models/TaskModel.ts
@@ -1,4 +1,31 @@
-import mongoose from "mongoose";
-const taskSchema = new mongoose.Schema(
+// models/Task.js
+import mongoose  from "mongoose";
 
-)
+const taskSchema = new mongoose.Schema({
+
+  text: {
+    type: String,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId, // Reference to User model
+    ref: 'User',
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+
+
+module.exports = mongoose.model('Task', taskSchema);
