aa480454925a0957808bcc657473df5c4ecd57da
SufiyanYounis <sufiyan.younis@mergestack.com>
2025-08-26 13:15:53 +0500

    refactor: set the casings

--- src/Features/TaskSlice.tsx
+++ src/Features/TaskSlice.tsx
@@ -1,32 +1,32 @@
 import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 
-type Tasks = {
+type tasks = {
   id: number;
   text: string;
   done: boolean;
 };
 type TasksState = {
-  items: Tasks[];
-  doneTask: number;
-  totalTask: number;
+  items: tasks[];
+  doneTasks: number;
+  totalTasks: number;
 };
 
-const initialState: TasksState = { items: [], doneTask: 0, totalTask: 0 };
+const initialState: TasksState = { items: [], doneTasks: 0, totalTasks: 0 };
 const taskSlice = createSlice({
   name: "task",
   initialState,
   reducers: {
-    addTask(state, action: PayloadAction<Tasks>) {
+    addTask(state, action: PayloadAction<tasks>) {
       state.items.push(action.payload);
-      state.totalTask += 1;
+      state.totalTasks += 1;
     },
     DeleteTask(state, action: PayloadAction<number>) {
       const toDelete = state.items.find((obj) => obj.id === action.payload);
       if (toDelete?.done) {
-        state.doneTask -= 1;
+        state.doneTasks -= 1;
       }
       state.items = state.items.filter((obj) => obj.id !== action.payload);
-      state.totalTask -= 1;
+      state.totalTasks -= 1;
     },
     EditTask(state, action: PayloadAction<{ id: number; text: string }>) {
       const updated_arr = state.items.find(
@@ -37,8 +37,8 @@ const taskSlice = createSlice({
     Toggle(state, action: PayloadAction<number>) {
       const updated_arr = state.items.find((obj) => obj.id === action.payload);
       if (updated_arr) updated_arr.done = !updated_arr.done;
-      if (updated_arr?.done) state.doneTask += 1;
-      else state.doneTask -= 1;
+      if (updated_arr?.done) state.doneTasks += 1;
+      else state.doneTasks -= 1;
     },
   },
 });
